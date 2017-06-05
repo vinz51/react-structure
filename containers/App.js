fb3bbe8caec903dd76bb126e6198e3ed245a6f85
vinz51 <vincent1.benoit@laposte.net>
2017-06-06 00:35:36 +0200

    Add the code for the form with redux.

--- containers/App.js
+++ containers/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 
+import Form from 'Containers/core/Form'
+import Input from 'Containers/core/Input'
+
 class App extends Component {
 
     render() {
         return(
             <h1>
                 Hello world !
+                <Form>
+                    <Input />
+                </Form>
             </h1>
         )
 
