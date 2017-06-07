8a9ce0eaf83dbc3738d714504ffb54c0085c4326
vinz51 <vincent1.benoit@laposte.net>
2017-06-07 17:53:37 +0200

    Remove the teststore

--- reducers/index.js
+++ reducers/index.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import { combineReducers } from 'redux'
 
-import testStore from './testStore'
-
 import form from './core/form'
 
 const reducersApp = combineReducers({
-    testStore,
     form
 })
 
